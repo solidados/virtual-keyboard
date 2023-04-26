5caa6a06215dd4b830b5e24ac3c34804822c4698
solidados <geekbrains.pk@internet.ru>
2023-04-26 17:30:58 +0400

    feat: add .eslintrc.js and its dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'airbnb-base',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  rules: {
+    'import/extensions': 0,
+    'import/prefer-default-export': 0,
+  },
+};
