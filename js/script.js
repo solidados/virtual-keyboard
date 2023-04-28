fe9cdc462c17dd312b1d7df6a2900ec867f8a410
solidados <geekbrains.pk@internet.ru>
2023-04-28 20:22:40 +0400

    feat: add keys to keyboard

--- js/script.js
+++ js/script.js
@@ -13,32 +13,29 @@ const description = createElement('p', 'description');
 pageElem.prepend(wrapper);
 wrapper.append(title, textarea, keyboard, description);
 textarea.id = 'textArea';
-// keyboard.append(row);
+
 description.innerText = 'This virtual keyboard was created in MacOS.\nTo switch languages press Fn';
 
 function createRow() {
-  for (let i = 0; i < KEY_CONTENT.length; i += 1) {
+  for (let i = 0; i < KEY_CONTENT.length - 1; i += 1) {
     const row = createElement('div', 'row');
     keyboard.append(row);
-    for (let j = 0; j < KEY_CONTENT[i].length; j += 1) {
-      const asciiCode = KEY_CONTENT[i][j];
-      const symbol = String.fromCharCode(asciiCode);
-      const key = createElement('button', 'key', symbol);
 
-      // key.classList.add(KEY_CONTENT[i][j][0]);
+    for (let j = 0; j < KEY_CONTENT[i].length; j += 1) {
+      const key = createElement('button', `key ${ KEY_CONTENT[i][j][0] }`);
 
       key.insertAdjacentHTML(
         'afterBegin',
-        `<div class='rus'>
-            <span class='caseDown'>${ KEY_CONTENT[i][j] }</span>
+        `<div class='rus hidden'>
+            <span class='caseDown'>${ KEY_CONTENT[i][j][1] }</span>
             <span class='caseUp hidden'>${ KEY_CONTENT[i][j][2] }</span>
-         </div>
-         <div class='eng hidden'>
+        </div>
+        <div class='eng'>
             <span class='caseDown hidden'>${ KEY_CONTENT[i][j][3] }</span>
             <span class='caseUp hidden'>${ KEY_CONTENT[i][j][4] }</span>
-         </div>`,
+        </div>`,
       );
-      row.appendChild(key);
+      row.append(key);
 
       row.append(key);
     }
@@ -46,29 +43,18 @@ function createRow() {
 }
 createRow();
 
-function makeRowOne() {
-  const rowOne = createElement('div', 'row__one row');
-  for (let i = 0; i < KEY_CONTENT.rowOne.length; i += 1) {
-    const asciiCode = KEY_CONTENT.rowOne[i];
-    const symbol = String.fromCharCode(asciiCode);
-    const button = createElement('button', 'key', symbol/* `${ symbol }` */);
-    keyboard.append(rowOne);
-    rowOne.append(button);
-  }
-}
+// function makeRowOne() {
+//   const rowOne = createElement('div', 'row__one row');
+//   for (let i = 0; i < KEY_CONTENT.rowOne.length; i += 1) {
+//     const asciiCode = KEY_CONTENT.rowOne[i];
+//     const symbol = String.fromCharCode(asciiCode);
+//     const button = createElement('button', 'key', symbol/* `${ symbol }` */);
+//     keyboard.append(rowOne);
+//     rowOne.append(button);
+//   }
+// }
 // makeRowOne();
 
-function makeRowTwo() {
-  const rowTwo = createElement('div', 'row__two row');
-  for (let i = 0; i < KEY_CONTENT.rowTwo.length; i += 1) {
-    const asciiCode = KEY_CONTENT.rowTwo[i];
-    const symbol = String.fromCharCode(asciiCode);
-    const button = createElement('button', 'key', symbol/* `${ symbol }` */);
-    keyboard.append(rowTwo);
-    rowTwo.append(button);
-  }
-}
-makeRowTwo();
 // const arr = [];
 // window.addEventListener('keydown', (UIEvent) => {
 //   const charCode = UIEvent.which;
